bf93e870e19dbe0e59e80da15304f230f41bd927
Long Ly <lymanhlong@gmail.com>
2020-04-15 19:04:38 -0400

    Initial Commit

---
+++ DateStructureProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DateStructureProject
+//
+//  Created by Long Ly on 2020-04-15.
+//  Copyright © 2020 Long Ly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
